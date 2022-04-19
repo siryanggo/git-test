bf6625a11d1f4f3215d50471383b86488f7b696c
12栋奥特曼 <1106367655@qq.com>
2022-04-19 21:39:13 +0800

    第一次提交

---
+++ src/main/java/com/yang/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.yang.git;
+
+/**
+ * @auther yhjStart
+ * @create 2022-04-19 21:29
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hellogit");
+    }
+}
